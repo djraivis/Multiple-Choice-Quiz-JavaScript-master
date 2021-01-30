fc316ee89e9cd3e0338950c8bff1856557f88e6a
Raivis Gereiss <highdoven@gmail.com>
2021-01-31 01:23:22 +0200

    first commit

---
+++ quiz.js
@@ -0,0 +1,223 @@
+// select all elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
+
+// create our questions
+let questions = [
+    {
+        question : "What is the Sparrow?",
+        imgSrc : "img/html.png",
+        choiceA : "Kamome",
+        choiceB : "Tsubame",
+        choiceC : "Suzume",
+        correct : "C"
+    },{
+        question : "How would you say おはようございます in Latvian?",
+        imgSrc : "img/css.png",
+        choiceA : "Labvakar",
+        choiceB : "Hyper Hyper",
+        choiceC : "Labrīt",
+        correct : "C"
+    },{
+        question : "River in Riga?",
+        imgSrc : "img/css.png",
+        choiceA : "Shinano",
+        choiceB : "Daugava",
+        choiceC : "Thames",
+        correct : "B"
+    },{
+        question : "How would you say KANPAI in Latvian?",
+        imgSrc : "img/css.png",
+        choiceA : "Dzeram",
+        choiceB : "Paldies",
+        choiceC : "Priekā",
+        correct : "C"
+    },{
+        question : "What time we normally start our video talk?",
+        imgSrc : "img/css.png",
+        choiceA : "4am",
+        choiceB : "8pm",
+        choiceC : "10pm",
+        correct : "C"
+    },{
+        question : "Largest city in the world?",
+        imgSrc : "img/css.png",
+        choiceA : "New York",
+        choiceB : "Tokyo",
+        choiceC : "Sao Paulo",
+        correct : "B"
+    },{
+        question : "NHL Ice-Hockey Team in Calgary / Canada?",
+        imgSrc : "img/css.png",
+        choiceA : "Calgary Senators",
+        choiceB : "Calgary Penguins",
+        choiceC : "Calgary Flames",
+        correct : "C"
+    },{
+        question : "Itadakimasu in Latvian?",
+        imgSrc : "img/css.png",
+        choiceA : "Esmu priecīga",
+        choiceB : "Labu apetīti",
+        choiceC : "Kiss me",
+        correct : "B"
+    },{
+        question : "Arigato in Latvian?",
+        imgSrc : "img/css.png",
+        choiceA : "Priekā",
+        choiceB : "Paldies",
+        choiceC : "Lūdzu",
+        correct : "B"
+    },{
+        question : "Have a nice day?",
+        imgSrc : "img/css.png",
+        choiceA : "Lai tev jauka diena ☺",
+        choiceB : "Vai tu sapņoji šonakt?",
+        choiceC : "Labu apetīti",
+        correct : "A"
+    },
+];
+
+// create some variables
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; // 10s
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
+
+// render a question
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+start.addEventListener("click",startQuiz);
+
+// start quiz
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+}
+
+// render progress
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+// counter render
+
+function renderCounter(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+// checkAnwer
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+// answer is correct
+function answerIsCorrect(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+// answer is Wrong
+function answerIsWrong(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+// score render
+function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    // choose the image based on the scorePerCent
+    let img = (scorePerCent >= 80) ? "img/5.png" :
+              (scorePerCent >= 60) ? "img/4.png" :
+              (scorePerCent >= 40) ? "img/3.png" :
+              (scorePerCent >= 20) ? "img/2.png" :
+              "img/1.png";
+    
+    scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
